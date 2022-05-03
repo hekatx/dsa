632d88c9246747a75c614a6d6b1d16b663398953
eka <phyzheye01@gmail.com>
2022-05-03 17:15:11 -0600

    feat(list): refactored test to use randomly generated data. added testify.

---
+++ utils/utils.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString() string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	n := int(RandomInt(3, 10))
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
